4740f94a2a541c7044f17b9a0b1f37f8226670a1
Calvin <ca7vin@gmail.com>
2022-05-18 23:13:40 +0200

    boutons de connection done CVDG

--- resources/views/front/partials/header.blade.php
+++ resources/views/front/partials/header.blade.php
@@ -13,13 +13,13 @@
                     <li><i class="fa fa-envelope-o"></i>your@website.com</li>
                     <li class="language">
                         <p><a href="#" id="example-show" class="showLink"
-                                onclick="showHide('example');return false;"><i
-                                    class="fa fa-globe"></i>English<i class="fa fa-angle-down"></i></a>
+                                onclick="showHide('example');return false;"><i class="fa fa-globe"></i>English<i
+                                    class="fa fa-angle-down"></i></a>
                         </p>
                         <div id="example" class="more">
                             <p><a href="#" id="example-hide" class="hideLink"
-                                    onclick="showHide('example');return false;"><i
-                                        class="fa fa-globe"></i>English<i class="fa fa-angle-up"></i></a>
+                                    onclick="showHide('example');return false;"><i class="fa fa-globe"></i>English<i
+                                        class="fa fa-angle-up"></i></a>
                             </p>
                             <form method="get" id="blog-search" class="blog-search">
                                 <ul>
@@ -41,8 +41,36 @@
                     <li><a href="{{ Route('news') }}" class="">News</a></li>
                     <li><a href="{{ Route('contact') }}" class="">Contact</a></li>
                     <li><a href="#search"><i class="fa fa-search"></i></a></li>
-                </ul>
-            </nav>
+                    <li>
+                        @if (Route::has('login'))
+                        @auth
+                            <div style='display: flex; align-items: center; justify-content: space-around; margin-top: 20px;'>
+                                <a href="{{ url('/dashboard') }}" class='btn p-5'
+                                    style='color: white; background-color: #A12C2F'>Dashboard</a>
+                                <form action="{{ route('logout') }}" method='post' style='margin-left: 10px;'>
+                                    @csrf
+                                    {{-- @method('POST') --}}
+                                    <a href="route('logout')" onclick="event.preventDefault();
+                                    this.closest('form').submit();" class='btn p-5' style='color: white; background-color: #A12C2F'
+                                        type='submit'>Logout</a>
+                                </form>
+                            </div>
+                        @else
+                        <div style='display: flex; align-items: center; justify-content: space-around; margin-top: 20px;'>
+                            <a href="{{ route('login') }}" class='btn p-5'
+                                style='color: white; background-color: #A12C2F'>Log in</a>
+
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class='btn p-5'
+                                    style='color: white; background-color: #A12C2F; margin-left: 10px;'>Register</a>
+                            @endif
+                        </div>
+                        @endauth
+                        @endif
+                    </li>
         </div>
+        </ul>
+        </nav>
+    </div>
     </div>
 </header>
