c88fa3c0d4b98d192cf377ce26385da29cadca84
Calvin <ca7vin@gmail.com>
2022-05-20 15:29:56 +0200

    Users Register Done CVDG

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,7 +9,7 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('register') }}">
+        <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
             @csrf
 
             <!-- Name -->
@@ -43,6 +43,12 @@
                 <x-input id="password_confirmation" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required />
+            </div>
+
+
+            <div class='mt-4 flex flex-col items-center justify-center'>
+                <label class='' for=''>Image</label>
+                <input type="file" name='image'>
             </div>
 
             <div class="flex items-center justify-end mt-4">
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,11 @@
 @extends('layouts.app')
 @section('content')
     @include('back/partials/sidebar')
-    <section class="home-section">
+    <section class="home-section d-flex flex-column align-items-center justify-content-center">
+        <div class="d-flex flex-column align-items-center justify-content-center">
+            <img class='w-100' src="{{ asset('images/logo.png') }}" alt="">
+        </div>
         <div class="text">Dashboard</div>
+        <h2 class="mb-5">Vous êtes connecté en tant que <strong>{{ auth()->user()->name }}</strong> (<em>{{ auth()->user()->role->name }})</em></h2>
     </section>
 @endsection
